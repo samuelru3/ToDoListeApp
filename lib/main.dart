fbc3c0bec465f8a6b04475456e230ada4b8f3b2c
samuelru3 <samuelruebel@gmail.com>
2021-03-18 12:42:37 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,18 @@ class ToDo extends StatefulWidget {
 }
 
 class _ToDoState extends State<ToDo> {
-  List<String> products = ['Tomate', 'Käse', 'Lauch', 'Paprika' ,'Wein'];
-
+  List<String> products = ['Tomate', 'Käse', 'Lauch', 'Paprika', 'Wein'];
   void addItem(String item) {
     setState(() {
       products.add(item);
     });
     Navigator.of(context).pop();
+  }
+
+  void deleteItem(int key){
+    setState(() {
+      products.remove(key);
+    });
   }
 
   void newEntry() {
@@ -35,28 +40,30 @@ class _ToDoState extends State<ToDo> {
               style: TextStyle(
                   fontSize: 18.0,
                   fontWeight: FontWeight.w800,
-                  fontFamily: 'Open Sans' ,
-                  color: Colors.black54
-              ),
+                  fontFamily: 'Open Sans',
+                  color: Colors.black54),
             ),
             title: Text('Neuen ToDo Punkt eingeben:'),
           );
-        }
-    );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue[50],
       appBar: AppBar(
-        title: Text("To-Do-App"),
-        backgroundColor: Color.fromRGBO(35, 152, 185, 100),
+        title: Text("Samuel's To-Do-App"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
       ),
-      body:
-      ListView.builder(
+      body: ListView.builder(
         itemCount: products.length,
         itemBuilder: (context, i) {
-          return ToDoItem( products[i] );
+          return ToDoItem(
+              products[i],
+              () => deleteItem(i),
+          );
         },
       ),
       floatingActionButton: FloatingActionButton(
@@ -69,27 +76,28 @@ class _ToDoState extends State<ToDo> {
 
 class ToDoItem extends StatelessWidget {
   final String title;
-  const ToDoItem(this.title);
+  const ToDoItem(this.title, this.remove);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 22, vertical: 3),
-      color: Colors.black45,
+      //color: Colors.blue[200],
       child: ListTile(
         contentPadding: EdgeInsets.symmetric(vertical: 8.0),
-        tileColor: Colors.black54,
+        tileColor: Colors.blue[800],
         leading: Checkbox(
           value: false,
         ),
         title: Text(
           title,
           style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.w600,
-              color: Colors.black54),
+              fontSize: 18.0, fontWeight: FontWeight.w600, color: Colors.white),
         ),
-        trailing: Icon(Icons.delete_outline),
+        trailing: IconButton(
+          icon: Icon(Icons.delete_outline),
+          onPressed: () => remove(),
+        )
       ),
     );
   }
